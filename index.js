d7ff505fa442c188f45569148edcb304a4354318
Iago Carvalho <iagocarvalho@Iago-MacBook.local>
2023-06-21 19:09:10 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const pdf = require('pdf-parse');
+const express = require('express'); 
+
+const app = express();
+app.use(express.json())
+
+app.use("/", express.static("public"));
+
+app.post("/extract-phrase", async (req, res) => {
+    
+    async function readDir() {
+        const files = fs.readdirSync('./pdfs');  
+        return files;
+    }
+    
+    async function readFiles(file) {
+        return new Promise((resolve, reject) => {
+
+            let dataBuffer = fs.readFileSync(`pdfs/${file}`);
+            let expression = req.body.word;
+            
+            pdf(dataBuffer).then(function(data, err) {
+                const re = new RegExp(`[^.?!]*${expression}[^.]*\.`, 'g');
+                const pdfText = data.text.replace(/(\r\n|\n|\r|\t)/gm, "");
+                const found = pdfText.match(re);
+                
+                if(found != null){
+                    const foundReplaced = found.map((el, key) =>  (el.replace(/(\r\n|\n|\r|\t)/gm, "") ))
+                    resolve(foundReplaced);         
+                }
+            })
+            .catch(function(error){
+                reject(error)
+            })
+        })
+    }
+    
+    async function sendPhrases(){
+        let data = []
+        const files = await readDir(); 
+
+        const promises = files.map(async file => { 
+            const result = await readFiles(file)
+            data.push(...result)
+        });
+        await Promise.all(promises)
+        console.log (data);
+ 
+        res.send(data);
+    }
+    
+    await sendPhrases();
+});
+
+app.listen(3000)
+
+// problem when readFiles returns []. How can I send an empty array using this function? 
+ 
+
---
+++ public/app.js
@@ -0,0 +1,27 @@
+document.querySelector('#btnSearch').addEventListener("click", sendWord)
+
+async function sendWord() {    
+    const search = document.getElementById('search').value; 
+    const list = document.getElementById('fill_list');
+
+    let phrase_list = [];
+
+    fetch("/extract-phrase", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },    
+        body: JSON.stringify({word: search})
+    }).then(response => {
+        return response.json();
+    }).then(data => {
+        data.forEach(item => {
+            let li = document.createElement("li");
+            let node = document.createTextNode(item);
+            li.appendChild(node);
+            list.appendChild(li);
+        });
+    });   
+}    
+
+
